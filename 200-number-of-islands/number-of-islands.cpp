49aff573a9f09da2023668a3f66db2a2c23e5a9b
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-07-18 00:32:33 +0530

    Time: 25 ms (72.87%) | Memory: 16.1 MB (89.79%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0'; // mark visited
+
+        // explore all 4 directions
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j); // explore the full island
+                }
+            }
+        }
+
+        return count;
+    }
+};
