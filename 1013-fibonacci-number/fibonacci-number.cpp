ec563887333889ffcfb17a811432d4dfaf43a227
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-08-22 01:26:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+    vector<int> dp;  // declare globally inside class
+
     int fib(int n) {
-        
-        if(n==0) return 0;
-        else if(n==1) return 1;
-        else  return(fib(n-1)+fib(n-2));
-        }
+        // resize dp for n+1 entries, filled with -1
+        if (dp.empty()) dp.resize(n + 1, -1);
+
+        if (n == 0 || n == 1) 
+            return n;
+
+        if (dp[n] != -1) 
+            return dp[n];
+
+        dp[n] = fib(n - 1) + fib(n - 2);
+        return dp[n];
+    }
 };
