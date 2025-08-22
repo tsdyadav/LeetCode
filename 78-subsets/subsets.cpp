690e150cc93980fad8ddf50c023a24167341a3d7
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-08-22 12:45:35 +0530

    Time: 4 ms (11.72%) | Memory: 15.7 MB (11.54%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-void subsequence(vector<int>&nums,int index,vector<vector<int>>&ans,vector<int>temp)
+void subseq(vector<int>& nums,int index,vector<vector<int>>&ans,vector<int> temp)
 {
     if(index==nums.size())
     {
         ans.push_back(temp);
         return;
     }
-    // if we want to not add the element then
-    subsequence(nums,index+1,ans,temp);
-    // if you want to add this element then
+    subseq(nums,index+1,ans,temp);
     temp.push_back(nums[index]);
-    subsequence(nums,index+1,ans,temp);
+    subseq(nums,index+1,ans,temp);
+    temp.pop_back();
 }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int>temp;
-        int n=nums.size();
-        subsequence(nums,0,ans,temp);
+         vector<vector<int>>ans;
+        vector<int> temp;
+        subseq(nums,0,ans,temp);
         return ans;
+        
     }
 };
