fdb50b6816f9fdffe3ff6b5546dea88251677308
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-09-05 22:34:25 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (60.64%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -0,0 +1,17 @@
+class NumArray {
+private:
+    vector<int> prefix;
+
+public:
+    NumArray(vector<int>& nums) {
+        int n = nums.size();
+        prefix.resize(n + 1, 0);  // prefix[0] = 0
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
