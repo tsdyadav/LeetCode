2bd62eae7d18039f7af93e71ad345bba48ca313c
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-10-26 16:14:33 +0530

    Time: 12 ms (12.65%) | Memory: 127.5 MB (25.22%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,54 +11,23 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-
-        if(head->next==NULL)
-        return 1;
-        int count=0;
+        stack<int>st;
         ListNode* temp=head;
-        while(temp)
+        while(temp!=nullptr)
         {
-            count++;
+            st.push(temp->val);
             temp=temp->next;
-             
         }
-        count/=2;
-        ListNode* curr=head, *prev=NULL;
-        //skip number of node
-        while(count--)
+
+        ListNode* curr=head;
+        while(curr!=nullptr)
         {
-            prev=curr;
+            if(curr->val!=st.top())
+            return false;
+            else 
+            st.pop();
             curr=curr->next;
         }
-        prev->next=NULL;
-        
-        // reverse the current linkedlist
-        ListNode *front;
-        prev=NULL;
-        while(curr)
-        {
-            front= curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=front;
-        } 
-
-        // prev is pointin to second list
-        // head is pointin to  first list
-        ListNode *head1=head, *head2=prev;
-        //check pallindrome
-        while(head1)
-        {
-            if(head1->val!=head2->val)
-            return 0;
-            else{
-                head1=head1->next;
-                head2=head2->next;
-            }
-            
-        }
-        return 1;
-
-        
+        return true;
     }
 };
