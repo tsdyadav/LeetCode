0115fabd0286b867de253ecfdebf27b63455a68c
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2025-12-07 00:01:41 +0530

    Time: 0 ms (100.00%) | Memory: 35.9 MB (30.08%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -4,11 +4,11 @@ public:
         int n=nums.size();
         int total=0;
         for(int num:nums)
-        total+=num;
-        int left=0;
+        total+=num; // total sum //28
+        int left=0; 
         for(int i=0;i<n;i++)
         {
-            int right=total-left-nums[i];
+            int right=total-left-nums[i];  
             if(left==right)
             return i;
             left+=nums[i];
