7e1b3d288073ac97e51fe291107146891c04cba9
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-01-27 15:20:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (16.35%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-       vector<int>lower(26,0),upper(26,0);
-       int count=0;
-       int odd=0;
-       for(int i=0;i<s.size();i++)
-       {
-        if(s[i]>='a')
+      unordered_map<char, int>mp;
+      for(char c : s)
+      mp[c]++;
+      int len=0;
+      bool odd=false;
+      for( auto it : mp)
+      {
+        if(it.second%2 == 0)
         {
-            lower[s[i]-'a']++;
+            len += it.second;
         }
         else
-        upper[s[i]-'A']++;
+        {
+            len+=it.second-1;
+            odd=true;
+        }
+      }
+      return odd? len +1  : len;
 
-       } 
-       for(int i=0;i<26;i++)
-       {
-        if(lower[i]%2==0)
-        count+=lower[i];
-        else
-        {
-            count+=lower[i]-1;
-            odd=1;
-        }
-         if(upper[i]%2==0)
-         count+=upper[i];
-        else
-        {
-            count+=upper[i]-1;
-            odd=1;
-        }
-       }
-
-      return count+odd; 
     }
 };
