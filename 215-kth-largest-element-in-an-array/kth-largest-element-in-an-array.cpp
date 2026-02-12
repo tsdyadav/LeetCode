99de1482e7bae7d6d381ddd6cf40055a6fdb0fec
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-02-12 11:30:30 +0530

    Time: 40 ms (26.03%) | Memory: 64.9 MB (5.66%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-       priority_queue< int, vector<int>, greater<int>> minheap;
-       for(int num: nums)
-       {
-       minheap.push(num);
-       if(minheap.size()>k)
-       {
-        minheap.pop();
-       }
-       }
-       return minheap.top();
-       }
+        priority_queue<int>pq;
+        for(auto &p : nums)
+        {
+            pq.push(p);
+        }
+        int result;
+        while(k>0)
+        {
+            result=pq.top();
+            pq.pop();
+            k--;
+        }
+        return result;
+    }
 };
