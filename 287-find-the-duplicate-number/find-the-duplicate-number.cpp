61ed270edec86b3c72bbec5bb6ff2ec31010c54f
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-02-16 12:43:49 +0530

    Time: 75 ms (11.06%) | Memory: 87.8 MB (15.79%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-  int findDuplicate(vector<int>& nums) {
-    int slow = nums[0];
-    int fast = nums[0];
+    int findDuplicate(vector<int>& nums) {
 
-    // Phase 1: Find intersection point
-    do {
-        slow = nums[slow];//3 4 2
-        fast = nums[nums[fast]]; //2 2
-    } while (slow != fast);
+       unordered_set<int>st;
+       for(auto &num : nums)
+       {
+        if(st.find(num) != st.end())
+        return num;
+        else
+        st.insert(num);
+        }
 
-    // Phase 2: Find entrance of cycle
-    slow = nums[0];
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[fast];
+       
+        return -1;
+        
     }
-    return slow;
-}
-
 };
