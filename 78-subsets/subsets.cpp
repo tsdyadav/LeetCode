64dbe149474a2f0968eb2fab42a2126c3539288e
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-03-11 08:30:27 +0530

    Time: 3 ms (29.88%) | Memory: 15.6 MB (15.62%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-void solver(int i,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans)
-{
-    if(i==nums.size())
+    void solve(int idx,vector<int>&nums,vector<int>temp,vector<vector<int>>&ans)
     {
-        ans.push_back(temp);
-        return;
-    }
-    // pick
-    temp.push_back(nums[i]);
-    solver(i+1,nums,temp,ans);
-    //backtrak
-    temp.pop_back();
-    //not pikking
-    solver(i+1,nums,temp,ans);
+        if(idx==nums.size())
+        {
+            //core logic
+            ans.push_back(temp);
+            return;
+        }
+        //pick
+        temp.push_back(nums[idx]);
+        solve(idx+1,nums,temp,ans);
+        //backtraking
+        temp.pop_back();
 
-}
+        // notpick
+        solve(idx+1,nums,temp,ans);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        solver(0,nums,temp,ans);
-        return ans;
+      vector<vector<int>> ans;
+      vector<int>temp;
+      solve(0,nums,temp,ans);
+      return ans;  
     }
 };
