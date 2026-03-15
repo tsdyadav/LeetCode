8aae0452e6c288f81e998bc6661f4337bf13affd
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-03-15 14:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.54%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,34 +1,24 @@
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        
-        for(char ch : s) {
-            
-            // If opening bracket, push to stack
-            if(ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            }
-            else {
-                // If stack empty, invalid
-                if(st.empty()) return false;
-                
-                char top = st.top();
+        for(char ch : s)
+        {
+
+            if(ch=='{'||ch=='['||ch=='(')
+            st.push(ch);
+            else
+            {
+                 if(st.empty()) return false;
+                if(ch=='}'&& st.top()!='{' ) return false;
+                if(ch==']'&& st.top()!='[' ) return false;
+                if(ch==')'&& st.top()!='(') return false;
                 st.pop();
-                
-                // Check matching
-                if((ch == ')' && top != '(') ||
-                   (ch == '}' && top != '{') ||
-                   (ch == ']' && top != '[')) {
-                    return false;
-                }
             }
+            
         }
-        
-        // If stack empty at end → valid
         return st.empty();
     }
+    
+    
 };
