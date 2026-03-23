b1e88939703244bb3aeaab0425efcfb4f9825c17
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-03-23 23:40:12 +0530

    Time: 13 ms (11.58%) | Memory: 26.4 MB (15.85%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 1; // different initially
-
-        // Step 1: Find candidates
-        for(int num : nums) {
-            if(num == candidate1) count1++;
-            else if(num == candidate2) count2++;
-            else if(count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            }
-            else if(count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            }
-            else {
-                count1--;
-                count2--;
+        unordered_map<int,int>freq;
+        vector<int>ans;
+        int n=nums.size()/3;
+        for(auto num: nums)
+        {
+            freq[num]++;
+        }
+        for(auto num: freq)
+        {
+            if(num.second>n)
+            {
+                ans.push_back(num.first);
             }
         }
-
-        // Step 2: Verify counts
-        count1 = count2 = 0;
-
-        for(int num : nums) {
-            if(num == candidate1) count1++;
-            else if(num == candidate2) count2++;
-        }
-
-        vector<int> result;
-
-        if(count1 > nums.size()/3) result.push_back(candidate1);
-        if(count2 > nums.size()/3) result.push_back(candidate2);
-
-        return result;
+        return ans;
     }
 };
