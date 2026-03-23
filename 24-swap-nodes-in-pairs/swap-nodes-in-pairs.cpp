dba690c6cd69502b6348158a9584d3959201698e
Dharampal Yadav <146646620+tsdyadav@users.noreply.github.com>
2026-03-24 01:23:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (19.70%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -1,26 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
-        ListNode dummy(0);
-        dummy.next = head;
-        
-        ListNode* prev = &dummy;
+        ListNode* dummy= new ListNode(0);
+        dummy->next=head;
 
-        while (prev->next && prev->next->next) {
-            
-            ListNode* first = prev->next;
-            ListNode* second = prev->next->next;
+        ListNode* prev= dummy;
+        while(prev->next && prev->next->next)
+        {
+            ListNode* first=prev->next;
+            ListNode* sec=prev->next->next;
+            first->next = sec->next;
+            sec->next = first;
+            prev->next = sec;
+            prev=first;
 
-            // Swapping
-            first->next = second->next;
-            second->next = first;
-            prev->next = second;
-
-            // Move prev forward
-            prev = first;
         }
+        return dummy->next;
 
-        return dummy.next;
     }
 };
